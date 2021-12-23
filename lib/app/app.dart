a5088145e0fb4dab60bb821d0374a6796adc0214
Suman Adhikari <expertxkid@gmail.com>
2021-12-23 11:36:23 +0545

    feat: update home screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,14 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:globalapp/screens/home.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState? of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale? _locale;
+
+  void setLocale(Locale value) {
+    setState(() {
+      _locale = value;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       supportedLocales: AppLocalizations.supportedLocales,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
+      locale: _locale,
       home: HomeScreen(),
     );
   }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:globalapp/app/app.dart';
+import 'package:globalapp/models/langauge.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var supportedLanguages = <Language>[
+    Language(1, "🇺🇸", "English", "en"),
+    Language(1, "🇺🇸", "English(UK)", "en", regionCode: 'GB'),
+    Language(2, "🇪🇸", "Spanish", "es"),
+  ];
+
+  void onSelect(Language item) {
+    switch (item.languageCode) {
+      case 'en':
+        print('English selected');
+        if (item.regionCode == 'GB') {
+          MyApp.of(context)?.setLocale(const Locale('en', 'GB'));
+        } else {
+          MyApp.of(context)?.setLocale(const Locale('en'));
+        }
+
+        break;
+      case 'es':
+        print('Spanish selected');
+        MyApp.of(context)?.setLocale(const Locale('es'));
+        break;
+
+      default:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    Locale myLocale = Localizations.localeOf(context);
+    print(myLocale);
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          PopupMenuButton<Language>(
+              onSelected: onSelect,
+              itemBuilder: (BuildContext context) {
+                return supportedLanguages.map((Language choice) {
+                  return PopupMenuItem<Language>(
+                    child: Text(choice.toString()),
+                    value: choice,
+                  );
+                }).toList();
+              }),
+        ],
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Text('Hello World!'),
+            child: Text(AppLocalizations.of(context)!.helloWorld),
           ),
           Center(
             child: Text('You\'ve got \$10 dollars!'),
